1a29f5aad3e53a02d2535e0e9eb38a57de029ea6
NuwanUdara <nuwanudara36@gmail.com>
2021-04-01 12:39:50 +0530

    Changed the algorithem completely so the changes were not staged

--- Microproducts/Audio Ouput/src/Audio_output/Audio_output.ino
+++ Microproducts/Audio Ouput/src/Audio_output/Audio_output.ino
@@ -1,47 +1,74 @@
-
 #include "pitches.h"
+int tempo = 225;
 
 int pinIn=13;
 int value=0;
+int pinIn2=12;
+int buzzer=8;
 
 
-int melody[] = {
-  NOTE_FS5, NOTE_FS5, NOTE_D5, NOTE_B4, NOTE_B4, NOTE_E5, 
-  NOTE_E5, NOTE_E5, NOTE_GS5, NOTE_GS5, NOTE_A5, NOTE_B5, 
-  NOTE_A5, NOTE_A5, NOTE_A5, NOTE_E5, NOTE_D5, NOTE_FS5, 
-  NOTE_FS5, NOTE_FS5, NOTE_E5, NOTE_E5, NOTE_FS5, NOTE_E5
+const int melody[] PROGMEM = {
+  NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8, //1
+  NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_B2, 8, NOTE_C3, 8,
+  NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8,
+  NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, -2,
+
+  NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8, //5
+  NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_B2, 8, NOTE_C3, 8,
+  NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8,
+  NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, -2,
+
+  NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8, //9
+  NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_B2, 8, NOTE_C3, 8,
+  NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8,
+  NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, -2,
+
+  NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8, //13
+  NOTE_C3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_AS2, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_B2, 8, NOTE_C3, 8,
+  NOTE_E2, 8, NOTE_E2, 8, NOTE_E3, 8, NOTE_E2, 8, NOTE_E2, 8, NOTE_D3, 8, NOTE_E2, 8, NOTE_E2, 8,
+  NOTE_FS3, -16, NOTE_D3, -16, NOTE_B2, -16, NOTE_A3, -16, NOTE_FS3, -16, NOTE_B2, -16, NOTE_D3, -16, NOTE_FS3, -16, NOTE_A3, -16, NOTE_FS3, -16, NOTE_D3, -16, NOTE_B2, -16,
+  
 };
 
-int durations[] = {
-  8, 8, 8, 4, 4, 4, 
-  4, 5, 8, 8, 8, 8, 
-  8, 8, 8, 4, 4, 4, 
-  4, 5, 8, 8, 8, 8
-};
+int notes = sizeof(melody) / sizeof(melody[0]) / 2;
 
-int songLength = sizeof(melody)/sizeof(melody[0]);
+int wholenote = (60000 * 4) / tempo;
+
+int divider = 0, noteDuration = 0;
+
 void setup() {
- pinMode(pinIn, INPUT); 
- 
-}
+   pinMode(pinIn, INPUT);
+   
+  }
 
 void loop() {
-  value=digitalRead(pinIn);
-  if (value==HIGH){
-      for (int thisNote = 0; thisNote < songLength; thisNote++){
-    // determine the duration of the notes that the computer understands
-    // divide 1000 by the value, so the first note lasts for 1000/8 milliseconds
-      int duration = 1000/ durations[thisNote];
-      tone(8, melody[thisNote], duration);
-    // pause between notes
-      int pause = duration * 1.3;
-      delay(pause);
-    // stop the tone
-      noTone(8);
-      value==LOW;
-  }
-    
-  }
+    value1=digitalRead(pinIn);
+    if (value==HIGH){
+        for (int thisNote = 0; thisNote < notes * 2; thisNote = thisNote + 2) {
 
+          // calculates the duration of each note
+          divider = pgm_read_word_near(melody+thisNote + 1);
+          if (divider > 0) {
+            // regular note, just proceed
+            noteDuration = (wholenote) / divider;
+          } 
+          else if (divider < 0) {
+            // dotted notes are represented with negative durations!!
+            noteDuration = (wholenote) / abs(divider);
+            noteDuration *= 1.5; // increases the duration in half for dotted notes
+           }
 
+           // we only play the note for 90% of the duration, leaving 10% as a pause
+           tone(buzzer, pgm_read_word_near(melody+thisNote), noteDuration * 0.9);
+
+           // Wait for the specief duration before playing the next note.
+          delay(noteDuration);
+
+          // stop the waveform generation before the next note.
+          noTone(buzzer);
+          
+          }
+      
+    }
+  
 }
