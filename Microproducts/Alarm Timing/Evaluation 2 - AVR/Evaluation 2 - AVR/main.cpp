58b26bb388aefaf64bcd697a92df2b0471e78c15
dakshinatharindu <tharinduokd@gmail.com>
2021-05-08 21:01:34 +0530

    create main code

---
+++ Microproducts/Alarm Timing/Evaluation 2 - AVR/Evaluation 2 - AVR/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * Evaluation 2 - AVR.cpp
+ *
+ * Created: 5/8/2021 9:00:23 PM
+ * Author : Dakshina Tharidndu
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
