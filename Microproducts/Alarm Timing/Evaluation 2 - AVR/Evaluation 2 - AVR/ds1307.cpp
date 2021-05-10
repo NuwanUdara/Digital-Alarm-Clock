e9bf590043ad29b62e3139d9453278d0df3ed72c
dakshinatharindu <tharinduokd@gmail.com>
2021-05-10 21:17:35 +0530

    define ds1307 address

--- Microproducts/Alarm Timing/Evaluation 2 - AVR/Evaluation 2 - AVR/ds1307.cpp
+++ Microproducts/Alarm Timing/Evaluation 2 - AVR/Evaluation 2 - AVR/ds1307.cpp
@@ -2,6 +2,8 @@
 #define DS1307_H
 #include "ds1307.h"
 #include <avr/io.h>
+#include "i2cmaster.h"
+#define DS1307_ADDRESS 0xD0
 
 uint8_t ds1307_dec2bcd(uint8_t val) {
 	return val + 6 * (val / 10);
