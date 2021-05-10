54a58d65c3eb49c021845361326f2e18cf50eb16
dakshinatharindu <tharinduokd@gmail.com>
2021-05-10 21:23:16 +0530

    add a function to set date

--- Microproducts/Alarm Timing/Evaluation 2 - AVR/Evaluation 2 - AVR/ds1307.cpp
+++ Microproducts/Alarm Timing/Evaluation 2 - AVR/Evaluation 2 - AVR/ds1307.cpp
@@ -11,4 +11,19 @@ uint8_t ds1307_dec2bcd(uint8_t val) {
 uint8_t ds1307_bcd2dec(uint8_t val) {
 	return val - 6 * (val >> 4);
 }
+void ds1307_setdate(uint8_t year, uint8_t month, uint8_t day, uint8_t dayofweek, uint8_t hour, uint8_t minute, uint8_t second) {
+	
+
+	i2c_start_wait(DS1307_ADDRESS | I2C_WRITE);
+	i2c_write(0x00);
+	i2c_write(ds1307_dec2bcd(second));
+	i2c_write(ds1307_dec2bcd(minute));
+	i2c_write(ds1307_dec2bcd(hour));
+	i2c_write(ds1307_dec2bcd(dayofweek));
+	i2c_write(ds1307_dec2bcd(day));
+	i2c_write(ds1307_dec2bcd(month));
+	i2c_write(ds1307_dec2bcd(year));
+	i2c_write(0x00);
+	i2c_stop();
+}
 #endif
