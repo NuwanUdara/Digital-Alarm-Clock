6dd59214aca83e58852443341bb9e128006a0e6d
dakshinatharindu <tharinduokd@gmail.com>
2021-05-12 01:06:26 +0530

    add get data function

--- Microproducts/Alarm Timing/Evaluation 2 - AVR/Evaluation 2 - AVR/ds1307.cpp
+++ Microproducts/Alarm Timing/Evaluation 2 - AVR/Evaluation 2 - AVR/ds1307.cpp
@@ -37,4 +37,19 @@ void ds1307_setdate(uint8_t year, uint8_t month, uint8_t day, uint8_t dayofweek,
 	i2c_write(0x00);
 	i2c_stop();
 }
+void ds1307_getdate(uint8_t *year, uint8_t *month, uint8_t *day, uint8_t *dayofweek, uint8_t *hour, uint8_t *minute, uint8_t *second) {
+	i2c_start_wait(DS1307_ADDRESS | I2C_WRITE);
+	i2c_write(0x00);
+	i2c_stop();
+
+	i2c_rep_start(DS1307_ADDRESS | I2C_READ);
+	*second = ds1307_bcd2dec(i2c_readAck() & 0x7F);
+	*minute = ds1307_bcd2dec(i2c_readAck());
+	*hour = ds1307_bcd2dec(i2c_readAck());
+	*dayofweek = ds1307_bcd2dec(i2c_readAck());
+	*day = ds1307_bcd2dec(i2c_readAck());
+	*month = ds1307_bcd2dec(i2c_readAck());
+	*year = ds1307_bcd2dec(i2c_readNak());
+	i2c_stop();
+}
 #endif
