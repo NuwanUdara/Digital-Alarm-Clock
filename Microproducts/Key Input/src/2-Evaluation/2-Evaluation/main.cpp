84f0f1a38e4ea2d4e7c7a6b1a194d5c3b1b20c67
DilshiTP <dilshipolgampala@gmail.com>
2021-05-12 15:56:50 +0530

    change the code to blink leds as a seven segment

--- Microproducts/Key Input/src/2-Evaluation/2-Evaluation/main.cpp
+++ Microproducts/Key Input/src/2-Evaluation/2-Evaluation/main.cpp
@@ -9,7 +9,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int key1,key2;
+int key1,key2,i;
 void keypad();
 
 int main(void)
@@ -35,7 +35,22 @@ void keypad(){
 	key2 = PINB & 0b01111000;
 	_delay_us(5);
 	
-	if (key1 == 0b00000100 && key2 == 0b00001000) { PORTD = 0b00100000; }
+	if (key1 == 0b00000100 && key2 == 0b00001000) { PORTD = 0b00100010; }
+	if (key1 == 0b00000010 && key2 == 0b00001000) { PORTD = 0b00011111; }
+	if (key1 == 0b00000001 && key2 == 0b00001000) { PORTD = 0b00110111; }
+	if (key1 == 0b00000100 && key2 == 0b00010000) { PORTD = 0b01100110; }
+	if (key1 == 0b00000010 && key2 == 0b00010000) { PORTD = 0b01110101; }
+	if (key1 == 0b00000001 && key2 == 0b00010000) { PORTD = 0b01111101; }
+	if (key1 == 0b00000100 && key2 == 0b00100000) { PORTD = 0b00100011; }
+	if (key1 == 0b00000010 && key2 == 0b00100000) { PORTD = 0b01111111; }
+	if (key1 == 0b00000001 && key2 == 0b00100000) { PORTD = 0b01100111; }
+	if ((key1 == 0b00000100 || key1 == 0b00000001) && key2 == 0b01000000) {
+		PORTD = 0b00000000;
+		for(i=0;i<7;i++){
+			PORTD |= (1<<i);
+		_delay_ms(100);}
+	}
+	if (key1 == 0b00000010 && key2 == 0b01000000) { PORTD = 0b01111011; }
 		
 }
 
