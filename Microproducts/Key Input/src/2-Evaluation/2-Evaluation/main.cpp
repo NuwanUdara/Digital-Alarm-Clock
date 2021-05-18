d058020ef15fcc7a9e9a4aebfd27c6863ed051ee
DilshiTP <dilshipolgampala@gmail.com>
2021-05-18 10:48:09 +0530

    change the keypad function and test simulation

--- Microproducts/Key Input/src/2-Evaluation/2-Evaluation/main.cpp
+++ Microproducts/Key Input/src/2-Evaluation/2-Evaluation/main.cpp
@@ -19,9 +19,9 @@ char key;
 
 int main(void)
 {
-	DDRD = 0b011111111;   /* Make all pins of PORTD as output pins */
 	while (1)
 	{
+		DDRD = 0b011111111;   /* Make all pins of PORTD as output pins */
 		key = keypad();
 		if (key == '1') { PORTD = 0b00100010; }
 		if (key == '2') { PORTD = 0b00011111; }
@@ -43,43 +43,29 @@ int main(void)
 }
 
 char keypad(){
-	DDRB = 0b01111111;
-	PORTB = 0b01111000;
+	while(1){
+		DDRB = 0b01111111;
+		PORTB = 0b01111000;
 	
-	key1 = PINB & 0b00000111;     /*get the pressed key*/
-	_delay_us(5);
+		key1 = PINB & 0b00000111;     /*get the pressed key*/
+		_delay_us(5);
 	
-	DDRB = 0b01111111;
-	PORTB = 0b00000111;
+		DDRB = 0b01111111;
+		PORTB = 0b00000111;
 	
-	key2 = PINB & 0b01111000;
-	_delay_us(5);
+		key2 = PINB & 0b01111000;
+		_delay_us(5);
 	
-	if (key1 == 0b00000001) {colloc = 2;}
-	else if (key1 == 0b00000010) {colloc = 1;}
-	else {colloc = 0;}
-		
-	if (key2 == 0b00001000) { return(keys[0][colloc]); }
-	else if (key2 == 0b00010000) { return(keys[1][colloc]); }
-	else if (key2 == 0b00100000) { return(keys[2][colloc]); }
-	else { return(keys[0][colloc]); }
+		if (key1 == 0b00000001) { colloc = 2; break; }
+		if (key1 == 0b00000010) { colloc = 1; break; }
+		if (key1 == 0b00000100) { colloc = 0; break; }
+	}
 	
-	//if (key1 == 0b00000100 && key2 == 0b00001000) { PORTD = 0b00100010; }
-	//if (key1 == 0b00000010 && key2 == 0b00001000) { PORTD = 0b00011111; }
-	//if (key1 == 0b00000001 && key2 == 0b00001000) { PORTD = 0b00110111; }
-	//if (key1 == 0b00000100 && key2 == 0b00010000) { PORTD = 0b01100110; }
-	//if (key1 == 0b00000010 && key2 == 0b00010000) { PORTD = 0b01110101; }
-	//if (key1 == 0b00000001 && key2 == 0b00010000) { PORTD = 0b01111101; }
-	//if (key1 == 0b00000100 && key2 == 0b00100000) { PORTD = 0b00100011; }
-	//if (key1 == 0b00000010 && key2 == 0b00100000) { PORTD = 0b01111111; }
-	//if (key1 == 0b00000001 && key2 == 0b00100000) { PORTD = 0b01100111; }
-	//if ((key1 == 0b00000100 || key1 == 0b00000001) && key2 == 0b01000000) {
-		//PORTD = 0b00000000;
-		//for(i=0;i<7;i++){
-			//PORTD |= (1<<i);
-		//_delay_ms(100);}
-	//}
-	//if (key1 == 0b00000010 && key2 == 0b01000000) { PORTD = 0b01111011; }
+	if (key2 == 0b00001000)  return(keys[0][colloc]); 
+	else if (key2 == 0b00010000)  return(keys[1][colloc]); 
+	else if (key2 == 0b00100000)  return(keys[2][colloc]); 
+	else if (key2 == 0b01000000)  return(keys[3][colloc]);
+	else return(0);
 		
 }
 
