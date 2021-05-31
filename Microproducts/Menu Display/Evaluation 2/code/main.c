ef7ce5c5184451fae7d6bee84647404e36e4d715
tharindu-123 <thenuwaratharindu325@gmail.com>
2021-05-31 23:39:00 +0530

    add sub menu

--- Microproducts/Menu Display/Evaluation 2/code/main.c
+++ Microproducts/Menu Display/Evaluation 2/code/main.c
@@ -9,25 +9,49 @@ void first(){
 	if(PINC & (1<<0)){
 		lcd_clrscr();
 		lcd_home();
-		lcd_puts("  MENU1");
+		lcd_puts(">>MENU1");
 		lcd_gotoxy(0,1);
-		lcd_puts(">>MENU2");
-			
+		lcd_puts("  MENU2");
+		_delay_ms(100);
+		if(PINC & (1<<1)){
+			lcd_clrscr();
+			lcd_home();
+			lcd_puts(">>SUB MENU 01");
+			lcd_gotoxy(0,1);
+			lcd_puts("  SUB MENU 02");
+			_delay_ms(100);
+			DDRC = ~DDRC & (1<<0);
+		}
 	}
 }
 void second(){
 	if(PINC & (1<<0)){
 		lcd_clrscr();
 		lcd_home();
+		lcd_puts("  MENU1");
+		lcd_gotoxy(0,1);
+		lcd_puts(">>MENU2");
+		_delay_ms(100);
+		DDRC = ~DDRC & (1<<0);
+			
+	}
+}
+void third(){
+	if(PINC & (1<<0)){
+		lcd_clrscr();
+		lcd_home();
 		lcd_puts("  MENU2");
 		lcd_gotoxy(0,1);
 		lcd_puts(">>MENU3");
+		_delay_ms(100);
+		DDRC = ~DDRC & (1<<0);
 		
 	}
 }
 int main(void)
 {
 	DDRC = DDRC & ~(1<<0);
+	DDRC = DDRC & ~(1<<1);
 	lcd_init(LCD_DISP_ON_CURSOR);
 	/* Replace with your application code */
 	lcd_clrscr();             /* clear screen of lcd */
@@ -41,10 +65,10 @@ int main(void)
 	
 	while (1)
 	{	
-		first();
-		_delay_ms(50);
 		second();
-		_delay_ms(50);
+		third();
+		first();
+		
 		
 		
 	
