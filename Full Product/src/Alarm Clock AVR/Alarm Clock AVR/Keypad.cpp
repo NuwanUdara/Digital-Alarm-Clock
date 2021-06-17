653807a188f1af4bef71c5a61f9d8f7154ed4aa0
dakshinatharindu <tharinduokd@gmail.com>
2021-06-17 16:33:50 +0530

    changed the ports of the keypad and checked.

--- Full Product/src/Alarm Clock AVR/Alarm Clock AVR/Keypad.cpp
+++ Full Product/src/Alarm Clock AVR/Alarm Clock AVR/Keypad.cpp
@@ -1,12 +1,11 @@
-
 #include "Keypad.h"
 #include <avr/io.h>
 #include <util/delay.h>
 #define keyPort PINC
 #define keyPin PIND
 char keys[4][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'},{'*','0','#'}};
-uint8_t row[4] = {PORTC0,PORTC1,PORTC2,PORTC3};
-uint8_t colomn[3] = {PIND4,PIND5,PIND6};
+uint8_t row[4] = {PORTD4,PORTD5,PORTD6,PORTD7};
+uint8_t colomn[3] = {PIND0,PIND1,PIND2};
 // default constructor
 Keypad::Keypad()
 {
@@ -19,11 +18,12 @@ Keypad::~Keypad()
 
 
 char btnPress(){
-	DDRC = 0x0F;
+	DDRD = 0xF0;
 	for (int i=0;i<4;i++){
-		PORTC = 1<<row[i];
+		PORTD = 1<<row[i];
+		//_delay_ms(500);
 		for (int j = 0;j<3;j++){
-			if (PIND==(1<<colomn[j])){
+			if ((PIND & 1<<colomn[j]) == (1<<colomn[j])){
 				//_delay_ms(100);
 				return keys[i][j];
 			}
